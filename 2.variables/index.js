ce01e90782d76d013d6c747b76cd02ae2c5b9350
jhenry <jhenrydeyvid2019@gmail.com>
2022-10-18 10:28:28 -0500

    iniciando el curso de javascript

---
+++ 2.variables/index.js
@@ -0,0 +1,46 @@
+//variables
+//una variable es un espacio donde almacenamos
+//un tipo de dato
+//para poder ser reutilizado
+
+//uso de una variable
+//palabra reservada es una instruccion propia
+//del lenguaje de programacion
+
+//palabra reservada para declarar una variable
+//existen 
+
+
+//let
+//para declarar un variable
+//se necesita 2 cosas
+//la palabra reservada y el nombre en donde quiero
+//almacenar el tipo de dato
+let nombre_de_pais
+var nombrePresidente
+let edadDeMiPerro
+
+//Variables asi nunca
+// let @hola
+// let 2hola
+// let -nombre
+// let x
+// let uno
+//proceso de asignacion
+//el nombre de la variable el signo de asignacion
+// y el tipo de dato a almacenar
+nombre_de_pais='peru'
+//las variables son mutables
+//que pueden ser reescritos
+nombre_de_pais='bolivia' //estoy reescribirndo el anterior dato 
+nombre_de_pais=15
+nombre_de_pais=true
+
+console.log(nombre_de_pais)
+
+//constantes
+const numeroDni=12345678
+//que a diferencia de una variable 
+//la constante no se puede mutar
+//cambiar el dato almacenado
+console.log(numeroDni)
