b757136fbf14cf0b5d14e00d2a98643513cdd687
jhenry <jhenrydeyvid2019@gmail.com>
2022-10-20 10:05:28 -0500

    jueves javascript grupal

---
+++ 3.operadores/ejercicios.js
@@ -0,0 +1,18 @@
+//OPERADOR DE ASIGNACION:
+mi variable="saludos"
+A+=B ejemplo equivalente:A = A+B
+A-=B ejemplo equivalente:A = A-B
+A/=B ejemplo equivalente:A = A/B
+A%=B ejemplo equivalente:A = A%B
+A*=B ejemplo equivalente:A = A*B
+//OPERADOR ARITMETICOS:
+suma(+). 3+2=5
+"A"+"B"="AB"
+"AB"+123="AB123"
+//OPERADORES LOGICOS
+console.log(true|| false);
+output: true
+console.log(!true);
+Output: false
+//OPERADORES DE COMPARACION.
+
---
+++ 3.operadores/index.js
@@ -0,0 +1,31 @@
+//OPERADORES DE JAVASCRIPT
+//OPERADORES DE ASIGNACION.
+//JHENRY:asigna un valor a su operando izquierdo basandose en el valor
+//de su operando derecho.
+//LUIS ANGEL:asigna un contenido a una variable o objeto.
+//CONCLUSION:tiene la particularidad de quye se puede combinarse con algunos de los operadores
+//aritmeticos,dando lugar a toda una familia de nuevos operadores.
+
+//OPERADORES ARITMETICOS.
+//JHENRY:es la suma de dos numeros y devuelve un solo valor numerico.
+//LUIS ANGEL:la suma de valores numericos ya sean literales o variables como sus operandos
+//CONCLUSION:los operadores aritmeticos son suma, resta,division,multiplicacion.
+
+//OPERADORES LOGICOS.
+//JHENRY:se utilizan con valores booleanos(logicos)cuando lo son devuelven un valor booleano.
+//LUIS ANGEL:nos permiten tomar decisiones mediante la verificacion de ese estado.
+//CONCLUSION:compara dos expresiones si se evalua como falsa o verdadera.
+
+//OPERADORES DE COMPARACION.
+//JHENRY:compara sus operandos y devuelven un valor logico.
+//LUIS ANGEL:verifica la igualdad de dos expresiones sin tener en cuenta el tipode dato.
+//CONCLUSION:comprueba si sus dos operandos son iguales y devuelve un resultado booleano.
+
+//CORREO:riverajorgelenny@gmail.com
+
+
+
+
+
+
+
