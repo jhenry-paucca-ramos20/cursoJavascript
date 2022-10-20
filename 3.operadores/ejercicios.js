57e683752c7a8b9bc309db570107eacddcbf2501
jhenry <jhenrydeyvid2019@gmail.com>
2022-10-20 10:52:10 -0500

    trabajo grupal javascript

--- 3.operadores/ejercicios.js
+++ 3.operadores/ejercicios.js
@@ -1,18 +1,51 @@
 //OPERADOR DE ASIGNACION:
-mi variable="saludos"
-A+=B ejemplo equivalente:A = A+B
-A-=B ejemplo equivalente:A = A-B
-A/=B ejemplo equivalente:A = A/B
-A%=B ejemplo equivalente:A = A%B
-A*=B ejemplo equivalente:A = A*B
+let a=10;
+console.log(a);
+Output:
+10
 //OPERADOR ARITMETICOS:
-suma(+). 3+2=5
-"A"+"B"="AB"
-"AB"+123="AB123"
+let a = 12;
+let b = 10;
+let result =a+b;
+console.log(result)
+console.log(1+2);
+console.log(a+6);
+Output
+22
+3
+18
 //OPERADORES LOGICOS
 console.log(true|| false);
 output: true
 console.log(!true);
 Output: false
 //OPERADORES DE COMPARACION.
-
+console.log(2==4);
+console.log("2"==2);
+Output:
+true
+false
+//OPERADORES DE CADENA.
+const numero1 =3;
+const numero2 =5;
+let resultado;
+resultado = numero1 > numero2;
+resultado = numero < numero2;
+resultado = numero >= numero2;
+resultado = numero <= numero2;
+//OPERADORES RELACIONALES
+var numero1 =3;
+var numero2 =5;
+resultado =numero1 > numero2; //resultado =false
+resultado =numero1 < numero2;//resultado= true
+//OPERADORES BITWISE
+// In binary-
+// 4: 100
+// 1: 001
+console.log(4 & 1):
+Output: 0
+//OPERADORES UNARIOS
+delete Object.property;
+delete Object[propertykey];
+delete objectName[index];
+delete property; // legal solo dentro de una declaracion with
